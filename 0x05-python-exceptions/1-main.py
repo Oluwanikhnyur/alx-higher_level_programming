b7ae2df5df77d75111d29ba1da7d06e0c1592e9b
oluwanikhnyur <oluwanikhnyur@gmail.com>
2024-01-23 01:15:01 -0800

    corrections

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -14,4 +14,4 @@ if not has_been_print:
 value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
-    print("{} is not an integer".format(value)
+    print("{} is not an integer".format(value))
